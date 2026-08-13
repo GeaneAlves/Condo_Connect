3b5a93953e15911f0a78e8a290217d4436f7dde3
Geane Alves <geanedarc2@hotmail.com>
2026-08-13 08:36:44 +0000

    V-2-backend

--- backendApp.py
+++ backendApp.py
@@ -14,8 +14,8 @@ def home():
 @app.route('/api/jardinagem', methods=['GET'])
 def get_jardinagem():
     eventos = [
-        {"id": 1, "atividade": "Poda das árvores e arbustos", "data", "2026-08-20", "status": "Agendado"};
-        {"id": 2, "Adubação do jardim frontal": "data", "2026-08-20", "status": "Agendado"}
+        {"id": 1, "atividade": "Poda das árvores e arbustos", "data": "2026-08-20", "status": "Agendado"},
+        {"id": 2, "atividade": "Adubação do jardim frontal", "data": "2026-08-20", "status": "Agendado"}
     ]
     return jsonify(eventos)
 
